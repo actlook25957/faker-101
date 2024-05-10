59f8ee3b80872e5303466e03cc00467e411a6ae2
Traitawat Jitchana <traitawat.25957@gmail.com>
2024-05-10 18:21:58 +0700

    [Created] gen user and add to json file

--- user.js
+++ user.js
@@ -1,12 +1,19 @@
 import { fakerTH, faker } from "@faker-js/faker";
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
-const thaiId = require('thaiid')
+const thaiId = require("thaiid");
+const fs = require("fs");
+
+// const firstRandom = faker.person.sex();
+// const fullNameEN = faker.person.fullName({ sex: firstRandom });
+// const fullNameTH = fakerTH.person.fullName({ sex: firstRandom });
 
 // Generate an array of fake users
-const users = Array.from({ length: 1 }, () => ({
-  fullNameEN: faker.person.fullName(),
+const users = Array.from({ length: 20 }, () => (
+  {
+  // fullNameEN: faker.person.fullName(),
   fullNameTH: fakerTH.person.fullName(),
+  sex: fakerTH.person.sex(),
   userId: faker.string.uuid(),
   username: faker.internet.userName(),
   email: faker.internet.email(),
@@ -14,9 +21,31 @@ const users = Array.from({ length: 1 }, () => ({
   password: faker.internet.password(),
   birthdate: faker.date.birthdate(),
   registeredAt: faker.date.past(),
-  address: faker.location.streetAddress(),
+  address: fakerTH.location.streetAddress(),
   phone: fakerTH.phone.number(),
-  thaiId: thaiId.random()
+  thaiId: thaiId.random(),
 }));
 
 console.log(users);
+
+var obj = {
+  users: users,
+};
+
+var json = JSON.stringify(obj);
+
+fs.writeFile('test.json', json, function (err) {
+  if (err) throw err;
+  console.log('Saved!');
+});
+
+// fs.readFile("myjsonfile.json", "utf8", function readFileCallback(err, data) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     obj = JSON.parse(data); //now it an object
+//     obj.table.push({ id: 2, square: 3 }); //add some data
+//     json = JSON.stringify(obj); //convert it back to json
+//     fs.writeFile("myjsonfile.json", json, "utf8", callback); // write it back
+//   }
+// });
